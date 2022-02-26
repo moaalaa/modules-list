619380f2224c295c79708d17192e414b14cc7a68
Mohamed Alaa El-Din <moaalaa16@gmail.com>
2022-02-26 14:30:08 +0200

    Fixing Styles

--- src/app/fake_data/modules.ts
+++ src/app/fake_data/modules.ts
@@ -3,25 +3,25 @@ import { Module } from "../Contracts/Module";
 export const MODULES: Module[] = [
     {
         id: 1,
-        icon: '/assets/img/modules-icons/shop.png',
+        icon: './assets/img/modules-icons/shop.png',
         title: 'E-Commerce',
         price: 12000,
     },
     {
         id: 2,
-        icon: '/assets/img/modules-icons/delivery.png',
+        icon: './assets/img/modules-icons/delivery.png',
         title: 'Delivery Express',
         price: 15000,
     },
     {
         id: 3,
-        icon: '/assets/img/modules-icons/portfolio.png',
+        icon: './assets/img/modules-icons/portfolio.png',
         title: 'Portfolio',
         price: 7000,
     },
     {
         id: 4,
-        icon: '/assets/img/modules-icons/casher.png',
+        icon: './assets/img/modules-icons/casher.png',
         title: 'Point Of Sale',
         price: 8000,
     },
